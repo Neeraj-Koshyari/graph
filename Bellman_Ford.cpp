492c4f594914c942dab0ef30eb9528241408c77d
Neeraj-Koshyari <140335273+Neeraj-Koshyari@users.noreply.github.com>
2023-11-08 19:52:33 +0530

    Create Bellman_Ford.cpp

---
+++ Bellman_Ford.cpp
@@ -0,0 +1,34 @@
+// LINK: https://practice.geeksforgeeks.org/problems/distance-from-the-source-bellman-ford-algorithm/0?fbclid=IwAR2_lL0T84DnciLyzMTQuVTMBOi82nTWNLuXjUgahnrtBgkphKiYk6xcyJU
+
+class Solution {
+  public:
+    /*  Function to implement Bellman Ford
+    *   edges: vector of vectors which represents the graph
+    *   S: source vertex to start traversing graph with
+    *   V: number of vertices
+    */
+    vector<int> bellman_ford(int V, vector<vector<int>>& edges, int S) {
+        vector<int> dis(V,1e8);
+        dis[S]=0;
+        
+        for(int i=0;i<V-1;i++){
+            for(auto& x:edges){
+                int u=x[0];
+                int v=x[1];
+                int wt=x[2];
+                
+                if(dis[u]!=1e8 && dis[u]+wt < dis[v])
+                    dis[v]=dis[u] + wt;
+            }
+        }
+        for(auto& x:edges){
+            int u=x[0];
+            int v=x[1];
+            int wt=x[2];
+            
+            if(dis[u]!=1e8 && dis[u]+wt < dis[v])
+                return {-1};
+        }
+        return dis;
+    }
+};
